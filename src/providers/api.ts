1dddf7aa507ca26f1f0288731d6b388e065f484f
gusflopes <gusflopes86@gmail.com>
2022-04-11 17:45:49 -0400

    wip: auth context

---
+++ src/providers/api.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import {parseCookies} from 'nookies'
+
+const cookies = parseCookies();
+
+export const api = axios.create({
+  baseURL: 'http://localhost:3334',
+  headers: {
+    Authorization: `Bearer ${cookies['pdfapp.accessToken']}`
+  }
+// })
+
+// import { AxiosInstance } from 'axios';
+// import {getAPIClient} from './axios'
+// export const api = getAPIClient();
---
+++ src/providers/axios.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosInstance } from 'axios'
+import {parseCookies} from 'nookies'
+import {HeadersDefaults} from 'axios'
+
+interface CommonHeaderProperties extends HeadersDefaults {
+  Authorization: string
+}
+
+interface IGetApiClient {
+  api: AxiosInstance
+}
+
+export function getAPIClient<IGetApiClient>(ctx?: any) {
+  const { 'pdfapp.accessToken': accessToken } = parseCookies(ctx)
+
+  const api = axios.create({
+    baseURL: 'http://localhost:3334'
+  })
+
+  api.interceptors.request.use((config) => {
+    console.log(config)
+    return config
+  })
+
+  if (accessToken) {
+    api.defaults.headers = {
+      Authorization: `Bearer ${accessToken}`
+    } as CommonHeaderProperties
+  }
+}
