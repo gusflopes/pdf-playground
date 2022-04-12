f88f80f45f0e7e93e2d0fdb1fdf11b1600207621
guflopes <gusflopes86@gmail.com>
2022-04-11 21:46:52 -0400

    feat: switch to nextjs

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,87 +1,92 @@
-import { AxiosResponse } from 'axios'
-import { setCookie, destroyCookie } from 'nookies'
-import {createContext, ReactNode, useState} from 'react'
-import { api } from '../providers/api'
-import {decode} from 'jsonwebtoken'
+import { AxiosResponse } from 'axios';
+import { setCookie, destroyCookie } from 'nookies';
+import { createContext, ReactNode, useState } from 'react';
+import { api } from '../providers/api';
+import { decode } from 'jsonwebtoken';
 
-type LoginCredentials ={
-  email: string
-  password: string
-}
+type LoginCredentials = {
+  email: string;
+  password: string;
+};
 
 type AuthContextData = {
-  login(credentials: LoginCredentials): Promise<void>
-  isAuthenticated: boolean
-  accessToken: string
-  refreshToken: string
-  refresh(): Promise<void>
-}
+  login(credentials: LoginCredentials): Promise<void>;
+  isAuthenticated: boolean;
+  accessToken: string;
+  refreshToken: string;
+  refresh(): Promise<void>;
+};
 
 type AuthProviderProps = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
-type LoginResponse ={
-  accessToken: string
-  refreshToken?: string
-}
+type LoginResponse = {
+  accessToken: string;
+  refreshToken?: string;
+};
 
-export const AuthContext = createContext({} as AuthContextData)
+export const AuthContext = createContext({} as AuthContextData);
 
-export function AuthProvider({children}: AuthProviderProps) {
+export function AuthProvider({ children }: AuthProviderProps) {
   // const [user, setUser] = UseState()
-  const [accessToken, setAcessToken] = useState('')
-  const [refreshToken, setRefreshToken] = useState('')
+  const [accessToken, setAcessToken] = useState('');
+  const [refreshToken, setRefreshToken] = useState('');
   const isAuthenticated = !!refreshToken;
 
   async function refresh() {
     try {
-      const response: AxiosResponse<LoginResponse> = await api.get('auth/refresh', {headers: {Authorization: `Bearer ${refreshToken}`}})
-      const {accessToken} = response.data
-      saveCookies({accessToken})
+      const response: AxiosResponse<LoginResponse> = await api.get(
+        'auth/refresh',
+        { headers: { Authorization: `Bearer ${refreshToken}` } }
+      );
+      const { accessToken } = response.data;
+      saveCookies({ accessToken });
     } catch (err) {
-      setAcessToken('')
-      setRefreshToken('')
-      destroyCookie(undefined, "pdfapp.accessToken")
-      destroyCookie(undefined, "pdfapp.refreshToken")
-      console.log(err)
+      setAcessToken('');
+      setRefreshToken('');
+      destroyCookie(undefined, 'pdfapp.accessToken');
+      destroyCookie(undefined, 'pdfapp.refreshToken');
+      console.log(err);
     }
   }
 
-  function saveCookies({accessToken, refreshToken}:LoginResponse) {
+  function saveCookies({ accessToken, refreshToken }: LoginResponse) {
     if (accessToken) {
-      console.log(decode(accessToken))
-      setAcessToken(accessToken)
-      setCookie(undefined, "pdfapp.accessToken", accessToken, {
-        maxAge: 60*3,
+      console.log(decode(accessToken));
+      setAcessToken(accessToken);
+      setCookie(undefined, 'pdfapp.accessToken', accessToken, {
+        maxAge: 60 * 3,
         path: '/',
-      })
+      });
     }
     if (refreshToken) {
-      setRefreshToken(refreshToken)
-      setCookie(undefined, "pdfapp.refreshToken", refreshToken, {
-        maxAge: 60*3,
+      setRefreshToken(refreshToken);
+      setCookie(undefined, 'pdfapp.refreshToken', refreshToken, {
+        maxAge: 60 * 3,
         path: '/',
-      })
+      });
     }
-
   }
 
-  async function login({email, password}:LoginCredentials) {
-    console.log({email, password})
+  async function login({ email, password }: LoginCredentials) {
+    console.log({ email, password });
     try {
-      const respose:AxiosResponse<LoginResponse> = await api.post('auth/login')
-      const { accessToken, refreshToken } = respose.data
-      saveCookies({accessToken, refreshToken})
-
+      const respose: AxiosResponse<LoginResponse> = await api.post(
+        'auth/login'
+      );
+      const { accessToken, refreshToken } = respose.data;
+      saveCookies({ accessToken, refreshToken });
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
   }
 
   return (
-    <AuthContext.Provider value={{ login, isAuthenticated, accessToken, refreshToken, refresh }}>
+    <AuthContext.Provider
+      value={{ login, isAuthenticated, accessToken, refreshToken, refresh }}
+    >
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
--- src/providers/axios.ts
+++ src/providers/axios.ts
@@ -1,30 +1,30 @@
-import axios, { AxiosInstance } from 'axios'
-import {parseCookies} from 'nookies'
-import {HeadersDefaults} from 'axios'
+import axios, { AxiosInstance } from 'axios';
+import { parseCookies } from 'nookies';
+import { HeadersDefaults } from 'axios';
 
 interface CommonHeaderProperties extends HeadersDefaults {
-  Authorization: string
+  Authorization: string;
 }
 
 interface IGetApiClient {
-  api: AxiosInstance
+  api: AxiosInstance;
 }
 
 export function getAPIClient<IGetApiClient>(ctx?: any) {
-  const { 'pdfapp.accessToken': accessToken } = parseCookies(ctx)
+  const { 'pdfapp.accessToken': accessToken } = parseCookies(ctx);
 
   const api = axios.create({
-    baseURL: 'http://localhost:3334'
-  })
+    baseURL: 'http://localhost:3333',
+  });
 
   api.interceptors.request.use((config) => {
-    console.log(config)
-    return config
-  })
+    console.log(config);
+    return config;
+  });
 
   if (accessToken) {
     api.defaults.headers = {
-      Authorization: `Bearer ${accessToken}`
-    } as CommonHeaderProperties
+      Authorization: `Bearer ${accessToken}`,
+    } as CommonHeaderProperties;
   }
 }
