eec4c6adca1432561275e07275abbd84bc071f5d
gusflopes <gusflopes86@gmail.com>
2022-04-13 11:55:38 -0400

    bug: Disclosure not working with React18 + strict mode

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // default is true - changed to false to fix Disclosure problems
+  reactStrictMode: false,
 };
 
 module.exports = nextConfig;
