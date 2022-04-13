a3ade9908ac73524d1248d74eb090976b159f832
gusflopes <gusflopes86@gmail.com>
2022-04-13 11:34:41 -0400

    feat: changed to tailwindcss

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,24 @@
-import '../styles/sass/globals.scss';
+import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '../context/AuthContext';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Add classes to HTML and Body
+    document.querySelector('html')?.classList.add('h-full');
+    document.querySelector('html')?.classList.add('bg-gray-100');
+    document.querySelector('body')?.classList.add('h-full');
+  }, []);
+
   return (
-    <>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </>
+    // <div className="h-full bg-gray-100">
+    // <div className="h-full">
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+    // </div>
+    // </div>
   );
 }
 
